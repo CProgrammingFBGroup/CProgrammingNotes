77ee46bdcf02f954b3c299ec31d3a64861eadede
Trenix25 <34799561+Trenix25@users.noreply.github.com>
2020-02-09 09:24:57 -0700

    Updated qsort_demo.c to also use pointers.

--- Examples/qsort_demo/qsort_demo.c
+++ Examples/qsort_demo/qsort_demo.c
@@ -17,9 +17,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* Define DEBUG to enable debugging output. */
+
+#undef DEBUG
+
 /* Define REVERSE_SORT_ORDER to sort in descending order. */
 
 #undef REVERSE_SORT_ORDER
+
+/* Define USE_POINTERS to use an array of character pointers. */
+
+#undef USE_POINTERS
 
 /* This is the prototype for the callback function: */
 
@@ -27,14 +35,91 @@ int compare( const void *arg1, const void *arg2 );
 
 int main( void )
 {
+
+#ifdef USE_POINTERS
+
+     char **words;
+     int pos2;
+
+#else
+
      /* Define the words array to be sorted: */
 
      char words[ 5 ][ 6 ] = { "one", "two", "three", "four", "five" };
+
+#endif  /* USE_POINTERS */
 
      /* limit is the size of the first dimension. */
      /* size is the size of the second dimension. */
 
      int limit = 5, pos, save_errno, size = 6;
+
+     /* Set up the array of character pointers. */
+
+#ifdef USE_POINTERS
+
+          /* Allocate memory for the array of pointers. */
+
+          errno = 0;
+          words = malloc( limit * sizeof( char * ) );
+          save_errno = errno;
+
+          /* See if that worked. */
+
+          if ( words == NULL )
+          {
+               printf( "\
+Failed to allocate memory for the array of pointers.\n" );
+               if ( save_errno != 0 )
+               {
+                    printf( "Error: %s.n", strerror( save_errno ) );
+               }
+               exit( EXIT_FAILURE );
+          }
+
+          /* Create a dynamically allocated character array for each spot. */
+
+          for( pos = 0; pos < limit; pos++ )
+          {
+               errno = 0;
+               words[ pos ] = malloc( size * sizeof( char ) );
+               save_errno = errno;
+
+               /* See if that worked. */
+
+               if ( words[ pos ] == NULL )
+               {
+                    printf( "\
+Failed to allocate memory for string position %d.\n", pos );
+                    if ( save_errno != 0 )
+                    {
+                         printf( "Error: %s.\n", strerror( save_errno ) );
+                    }
+
+                    /* Free the array of character pointers. */
+
+                    for( pos2 = 0; pos2 < pos; pos2++ )
+                    {
+                         free( words[ pos2 ] );
+                         words[ pos2 ] = NULL;
+                    }
+                    free( words );
+                    words = NULL;
+
+                    exit( EXIT_FAILURE );
+               }
+
+          }    /* for( pos = 0; pos < limit; pos++ ) */
+
+          /* Write the words into their respective character arrays. */
+
+          strcpy( words[ 0 ], "one" );
+          strcpy( words[ 1 ], "two" );
+          strcpy( words[ 2 ], "three" );
+          strcpy( words[ 3 ], "four" );
+          strcpy( words[ 4 ], "five" );
+          
+#endif  /* USE_POINTERS */
 
      /* Print the words array before sorting it. */
 
@@ -47,9 +132,19 @@ int main( void )
 
      /* Use qsort(3) to sort the words array. */
 
+#ifdef USE_POINTERS
+
+     errno = 0;
+     qsort( words, limit, sizeof( char * ), compare );
+     save_errno = errno;
+
+#else
+
      errno = 0;
      qsort( words, limit, size, compare );
      save_errno = errno;
+
+#endif  /* USE_POINTERS */
 
      /* See if anything went wrong. */
 
@@ -68,6 +163,20 @@ int main( void )
      }
      printf( "\n" );
 
+     /* Free the array of character pointers. */
+
+#ifdef USE_POINTERS
+
+     for( pos = 0; pos < limit; pos++ )
+     {
+          free( words[ pos ] );
+          words[ pos ] = NULL;
+     }
+     free( words );
+     words = NULL;
+
+#endif  /* USE_POINTERS */
+
      exit( EXIT_SUCCESS );
 }
 
@@ -75,6 +184,13 @@ int main( void )
 
 int compare( const void *arg1, const void *arg2 )
 {
+
+#ifdef USE_POINTERS
+
+     char **ptr;
+
+#endif
+
      char *str1, *str2;
      int ret;
 
@@ -88,12 +204,35 @@ int compare( const void *arg1, const void *arg2 )
 
      /* Type cast the strings so we can use them. */
 
+#ifdef USE_POINTERS
+
+     ptr = ( char ** )arg1;
+     str1 = *ptr;
+     ptr = ( char ** )arg2;
+     str2 = *ptr;
+
+#else
+
      str1 = ( char * )arg1;
      str2 = ( char * )arg2;
 
+#endif  /* USE_POINTERS */
+
      /* Compare the strings. */
 
+#ifdef DEBUG
+
+     printf( "Comparing \"%s\" to \"%s\".\n", str1, str2 );
+
+#endif
+
      ret = strcmp( str1, str2 );
+
+#ifdef DEBUG
+
+    printf( "ret: %d.\n", ret );
+
+#endif
 
      /* Use descending order if requested. */
 
